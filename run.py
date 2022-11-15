67f22cc8e90c6f273b2c9d7044d360a182bc7648
danio86 <d.harder@gmx.at>
2022-11-15 10:57:45 +0000

    add tyr/except block to prevent Invalid data

--- run.py
+++ run.py
@@ -30,6 +30,23 @@ def get_sales_data():
     print('Example: 10, 20 ,30, 40 ,50, 60\n')
 
     data_str = input('Enter your data here: ')
-    print(f'The data prvided is: {data_str}')
+    sales_data = data_str.split(',')
+    validate_data(sales_data)
+
+def validate_data(values):
+    """
+    converts values ti int, raises valueErrors 
+    if not 6 numbers or vales can't be converted
+    """
+    try:
+        if len(values) !=6:
+            raise ValueError(
+                f'Exactly 6 numbers! You provided {len(values)} numbers'
+            )
+    except ValueError as e:
+        """ e wird zu variable """
+        print(f'Invalid data: {e}! Please try again!\n')
+
+    print(values)
 
 get_sales_data()
