f04423b1cba7d522e241404fcc0c8d57a5de38f6
danio86 <d.harder@gmx.at>
2022-11-15 09:26:23 +0000

    wire up APIs and confirm acces to google sheet

--- run.py
+++ run.py
@@ -1,3 +1,22 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import gspread
+from google.oauth2.service_account import Credentials
+""" nur die KLasse (Credentials) aus der libary (google.oauth2.service_account) wird importiert """
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+""" Großgeschriebene Variabelen sind Konstante, die nicht verändert werden sollten """
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('love_sandwiches') 
+""" das ist der name vom EXCEL-file """
+
+sales = SHEET.worksheet('sales') 
+""" das ist eine sheet aus dem Ex-file """
+
+data = sales.get_all_values()
+print(data)
